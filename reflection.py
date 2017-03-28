7e6dc99a5c64b1b069e63dccf4304adeddb71b83
Max Kovalenko <mkovalenko@fusionconnect.com>
2017-03-28 22:42:48 +0300

    template files are appended for further development. Innitial Project file structure is created.

---
+++ reflection.py
@@ -0,0 +1,8 @@
+"""
+Project code: FCONF
+Development code: NCB-20
+File: REFLECTION.PY
+File location: ../flask-dev/
+type: Python 2.7
+Description: Program module containing API functions referred to as Reflection data in Web server features
+"""
