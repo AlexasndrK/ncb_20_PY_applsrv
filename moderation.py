3275cf4b010000791c1d6aeac4426dbf5a55f849
AlexasndrK <a6apmot@gmail.com>
2017-04-03 21:58:42 +0300

    unnecessary  file has been deleted

--- moderation.py
+++
@@ -1,8 +0,0 @@
-"""
-Project code: FCONF
-Development code: NCB-20
-File: MODERATION.PY
-File location: ../flask-dev/
-type: Python 2.7
-Description: Program module containing API functions referred to as Conference room moderation features
-"""
