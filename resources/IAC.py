2980d58d419e120ac6ad9bc729e5a3020a358597
Maxim L. Rollet <maximrollet@gmail.com>
2017-04-12 22:01:20 +0300

    My First finished PYTHON two functions

--- resources/IAC.py
+++ resources/IAC.py
@@ -12,13 +12,31 @@ from flask_restful import Resource, reqparse
 
 class User(Resource):  # POST - also can be used for GET and UPDATE
     def get(self, user):
-        pass
+        dbcon = db.ncbDB()
+        sql = 'SELECT id FROM users WHERE user={}'.format(user)
+        eslServer = dbcon.ncb_getQuery(sql)
+        if len(eslServer) == 0 or len(eslServer) > 1:
+            return{"result": False, "why": "I dont know.  I didnt code any debug here..."}
+        else:
+            id = eslServer["id"]
+            return{"result": True, "id": id}
 
     def post(self):
         pass
 
     def delete(self, user):
-        pass
+        dbcon = db.ncbDB()
+        sql1 = 'delete from users where user={}'.format(user)
+        eslServDel = dbcon.ncb_pushQuery(sql1)
+        if len(eslServDel) > 0:
+            sql2 = 'select id from users where user={}'.format(user)
+            eslServId = dbcon.ncb_getQuery(sql2)
+            id = eslServId["id"]
+            sql3 = 'delete from user2conf where userid=id'
+            eslServDel = dbcon.ncb_pushQuery(sql3)
+            return{"result": True}
+        else:
+            return{"result": False, "why": "Broke deleting user links"}
 
 
 class ResetUserPassword(Resource):  # POST
